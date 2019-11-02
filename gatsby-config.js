8189d5f1e77cbcf0734914136665d8487b2f8f6f
AbdelrhmanYousry <eng_abdelrhman93@yahoo.com>
2019-11-02 14:23:13 +0200

    update configs meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shootrz is coming soom`,
+    description: `The first on-deman photography service in MENA will be live, stay tuned.`,
+    author: `@shootrz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
