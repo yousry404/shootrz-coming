b532e011db4b256abff7be79ca53da7aae974560
AbdelrhmanYousry <eng_abdelrhman93@yahoo.com>
2019-11-23 09:23:41 +0200

    Update ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,4 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./src/css/layout.scss"
+import "./src/containers/login/index.scss"
+import "./src/views/events/styles.scss"
+import "./src/views/book/styles.scss"
+
+import "./src/css/typography.css";
 export { default as wrapRootElement } from './src/ReduxWrapper';
