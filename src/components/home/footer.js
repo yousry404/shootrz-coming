85782a8e4491bb07375edc9b507097edf5cd07c6
AbdelrhmanYousry <abdelrahman.yousry@admins-MacBook-Pro.local>
2020-05-23 06:30:48 +0200

    Added external link practice

--- src/components/home/footer.js
+++ src/components/home/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>Join our team of photographers</p>
-      <a href="https://photographer.shootrz.co/app/signup" target="_blank">Apply now</a>
+      <a href="https://photographer.shootrz.co/app/signup" rel="noopener" target="_blank">Apply now</a>
       <img src={footerLogo} alt="shootrz"/>
       <p className="copy">&copy;All copyrights reserved to Shootrz 2019</p>
     </div>
